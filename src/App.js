7734996edfcb54747cb3b56b15a39cd3abf7f397
larissa.rangel <larissa.rangel@MacBook-Air.local>
2023-01-06 15:29:53 -0300

    create movies service

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import { MoviesService } from './api/MoviesService';
 import { Header } from './components/Header';
 import { MoviesList } from './components/MoviesList';
 import { MoviesContainer } from './styles/MoviesContainer';
 
+import './App.css';
+
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    const { data } = await MoviesService.getMovies();
+    setMovies(data.results);
+  }
+
+  useEffect(() => {
+    fetchMovies(); 
+  }, [])
+
   return (
     <div className="App">
       <Header />
 
       <MoviesContainer>
-        <MoviesList movies={[]} />
+        <MoviesList movies={movies} />
       </MoviesContainer>
     </div>
   );
---
+++ src/api/MoviesService.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API_KEY = '6d60d7d80942a98a2c3241b43cb58dcb';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const withBaseUrl = path => `${BASE_URL}${path}?api_key=${API_KEY}`;
+
+export class MoviesService {
+    static getMovies() {
+        return axios(withBaseUrl('movie/popular'))
+    }
+}
